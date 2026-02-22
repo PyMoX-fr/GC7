b6ddbe010dc129b07b8492463b2b0f444efe0902
GrCOTE7 <grcote7@gmail.com>
2026-02-22 11:05:42 +0100

    up versioning auto

--- teste_write_version.py
+++ teste_write_version.py
@@ -1,9 +1,9 @@
 import re
 
-with open("gc7/__init__.py", "r+") as f:
+with open("src/pymox_tools1/__init__.py", "r+") as f:
     content = f.read()
     content = re.sub(
-        r'__version__\s*=\s*["\'].*?["\']', '__version__ = "1.1.0"', content
+        r'__version__\s*=\s*["\'].*?["\']', '__version__ = "0.10.0"', content
     )
     f.seek(0)
     f.write(content)
